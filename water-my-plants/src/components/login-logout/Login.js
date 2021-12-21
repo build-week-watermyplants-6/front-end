2d110805435cc82cdd77b08cecd0f5b5e23139dc
Bobby Gomez <bobbygomez@Bobbys-MacBook-Pro.local>
2021-12-20 23:50:03 -0600

    edited comments

--- water-my-plants/src/components/login-logout/Login.js
+++ water-my-plants/src/components/login-logout/Login.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+//import { Link } from 'react-router-dom';
+//import axios from 'axios';
 
 const Login = () => {
     return (
         <div className="">
-            <form className="" onSubmit={handleSubmit}>
+            <form className="" >
                 <h2 className="title">Enter your email and Password</h2>
                 <div className="inputContainer">
                     <label htmlFor="email" className="label">Email: </label>
                     <input 
                         type="text" 
                         name="email" 
-                        value={} 
-                        onChange={handleChange} 
+                        //value = {} 
+                        //onChange={handleChange} 
                         placeholder="Enter email..." 
                         className="input" 
                     />
@@ -23,18 +23,18 @@ const Login = () => {
                     <input 
                         type="password" 
                         name="password" 
-                        value={} 
-                        onChange={handleChange} 
+                        //value={} 
+                        //onChange={handleChange} 
                         placeholder="Enter password..." 
                         className="input" 
                     />
                 </div> 
                 <div>
-                    { loginFailed && <p className="error-msg">Email or password incorrect </p>}
+                    { /*loginFailed && <p className="error-msg">Email or password incorrect  </p> */}
                 </div>
                 <button type="submit" className="submitBtn" value="Log In">Log In</button>
                 <p>Don't have an account?</p>
-                <Link to='/signup'>Sign Up</Link>
+                {/* < Link to='/signup'>Sign Up</Link>  */}
             </form>
         </div>
     )
